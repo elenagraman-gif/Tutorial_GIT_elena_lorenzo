f841e02f2d6f6f5e9dc3a4582de479bdb1aef7c2
elenagraman-gif <elena.graman@sanviatorvalladolid.com>
2025-11-22 22:47:50 +0100

    Comentario 15, función 1 en archivoE creada

---
+++ src/proyectoEntorno/archivoE.java
@@ -0,0 +1,13 @@
+package proyectoEntorno;
+
+public class archivoE {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void funcion1E() {
+		System.out.println("Función 1 archivo E");
+	}
+}
