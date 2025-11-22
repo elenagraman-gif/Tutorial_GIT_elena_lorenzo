03bc199243ce1fb2b381b06c8c2da00bec682cee
lorenzorudgon-hash <lorenzo.rudgon@sanviatorvalladolid.com>
2025-11-22 21:59:17 +0100

    añadiendo actualizacion al repositorio remoto

---
+++ src/proyectoEntorno/archivoA.java
@@ -0,0 +1,43 @@
+package proyectoEntorno;
+
+public class archivoA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static void funcion1() {
+		System.out.println("Función 1");
+	}
+	
+	public static void funcion2() {
+		System.out.println("Función 2");
+	}
+	
+	public static void funcion3() {
+		System.out.println("Función 3");
+	}
+	
+	public static void funcion4() {
+		System.out.println("Función 4");
+	}
+	
+	public static void funcion5() {
+		System.out.println("Función 5");
+	}
+	
+	public static void funcion6() {
+		System.out.println("Función 6");
+	}
+	
+	public static void funcion7() {
+		System.out.println("Función 7");
+	}
+	
+	public static void funcion8() {
+		System.out.println("Función 8");
+	}
+	
+	
+}
