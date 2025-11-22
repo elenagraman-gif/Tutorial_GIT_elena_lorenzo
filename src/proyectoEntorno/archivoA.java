36f228ae1e2c816e0ee74e6cce9977eeb7e9ec11
elenagraman-gif <elena.graman@sanviatorvalladolid.com>
2025-11-22 20:24:02 +0100

    Comentario 2, función 2 creada

--- src/proyectoEntorno/archivoA.java
+++ src/proyectoEntorno/archivoA.java
@@ -6,5 +6,13 @@ public class archivoA {
 		// TODO Auto-generated method stub
 
 	}
-
+	
+	public static void funcion1() {
+		System.out.println("Función 1");
+	}
+	
+	public static void funcion2() {
+		System.out.println("Función 2");
+	}
+	
 }
