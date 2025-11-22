c738ca7151bce7f4bae63c8ff7ef52c7cc2a147d
lorenzorudgon-hash <lorenzo.rudgon@sanviatorvalladolid.com>
2025-11-22 22:49:33 +0100

    Creado el archivoF y primera funcionAF

---
+++ src/proyectoEntorno/archivoF.java
@@ -0,0 +1,13 @@
+package proyectoEntorno;
+
+public class archivoF {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public static void funcionAF() {
+		System.out.println("Funcion AF");
+	}
+
+}
