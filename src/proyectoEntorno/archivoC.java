ecfc65305fccf004afdeb35ad0abfbda428701e6
elenagraman-gif <elena.graman@sanviatorvalladolid.com>
2025-11-22 22:18:05 +0100

    Comentario 12, función 1 en archivoC creada

---
+++ src/proyectoEntorno/archivoC.java
@@ -0,0 +1,13 @@
+package proyectoEntorno;
+
+public class archivoC {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void funcion1C() {
+		System.out.println("Función 1 archivo C");
+	}
+}
