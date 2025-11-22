b57f044bb7f852542b6815216536f654bea91ab6
elenagraman-gif <elena.graman@sanviatorvalladolid.com>
2025-11-22 22:48:18 +0100

    Comentario 16, función 2 en archivoE creada

--- src/proyectoEntorno/archivoE.java
+++ src/proyectoEntorno/archivoE.java
@@ -10,4 +10,8 @@ public class archivoE {
 	public static void funcion1E() {
 		System.out.println("Función 1 archivo E");
 	}
+	
+	public static void funcion2E() {
+		System.out.println("Función 2 archivo E");
+	}
 }
