1d3e59fc3ac699268c26334affa7144acb21d146
lorenzorudgon-hash <lorenzo.rudgon@sanviatorvalladolid.com>
2025-11-22 22:18:52 +0100

    Creado archivoD y primera funcion AD

---
+++ src/proyectoEntorno/archivoD.java
@@ -0,0 +1,13 @@
+package proyectoEntorno;
+
+public class archivoD {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public static void funcionAD () {
+		System.out.println("Funcion AD");
+	}
+
+}
