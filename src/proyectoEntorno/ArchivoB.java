d5f18aa6962af0bc60dda4ca76b19817c1a986f2
lorenzorudgon-hash <lorenzo.rudgon@sanviatorvalladolid.com>
2025-11-22 20:30:00 +0100

    Creada la clase archivoB y primera funcion

---
+++ src/proyectoEntorno/ArchivoB.java
@@ -0,0 +1,12 @@
+package proyectoEntorno;
+
+public class ArchivoB {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public static void funcionA() {
+		System.out.println("Funcion A");
+	}
+}
